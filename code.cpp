81b181dbfaf6bf60b509edb4ad44b2570e1a75ce
Noorfatimadev <59045813+Noorfatimadev@users.noreply.github.com>
2021-09-14 19:55:14 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,23 @@
+#include<iomanip>
+#include<iostream>
+using namespace std; 
+  
+int main()
+{  
+  int n = 0;
+  int nextPowerOf2(n);  
+      
+   
+    if (n && !(n & (n - 1)))  
+      
+   {
+	     
+    while( n != 0)  
+    {  
+        n >>= 1;  
+        n=n+ 1;  
+    
+}  
+  cout << nextPowerOf2; }
+    return 0;
+}
